2817508a1401a8c557d4c7ba8871a7178c8da403
williamsj71169 <58008516+williamsj71169@users.noreply.github.com>
2022-02-10 10:36:50 +1300

    2

--- 01_first bits.py
+++ 01_first bits.py
@@ -1,5 +1,5 @@
-
-# import
+from tkinter import *
+import random
 
 
 class Start:
@@ -14,7 +14,66 @@ class Start:
         self.starting_funds.set(0)
 
         # Mystery Heading (row 0)
-        self.mystery_box
+        self.mystery_box_label = Label(self.start_frame, text="Mystery Box Game",
+                                       font="Arial 19 bold")
+        self.mystery_box_label.grid(row=0)
+
+        # initial instructions (row 1)
+        self.mystery_instructions = Label(self.start_frame,
+                                          font="Arial 10 italic",
+                                          text="Please enter a dollar amount (between $5 and $50) in the box below."
+                                               "Then click the 'Add Funds' button and choose the stakes."
+                                               "The higher the stakes, the more you can win!",
+                                          wrap=275, justify=LEFT, padx=10, pady=10)
+
+        self.mystery_instructions.grid(row=1)
+
+        # Entry box, button & Error Label (row 2)
+        self.entry_error_frame = Frame(self.start_frame, width=200)
+        self.entry_error_frame.grid(row=2)
+
+        self.start_amount_entry = Entry(self.entry_error_frame, font="Arial 19 bold", width=10)
+        self.start_amount_entry.grid(row=0, column=0)
+
+        self.add_funds_button = Button(self.entry_error_frame,
+                                       font="Arial 14 bold", text="Add Funds", command=self.check_funds)
+
+        self.add_funds_button.grid(row=0, column=1)
+
+        self.amount_error_label = Label(self.entry_error_frame, fg="maroon", text="", font="Arial 10 bold",
+                                        wrap=275, justify=LEFT)
+        self.amount_error_label.grid(row=1, columnspan=2, pady=5)
+
+        # button frame (row 3)
+
+
+    def check_funds(self):
+        starting_balance = self.start_amount_entry.get()
+
+        # Set error background colours (and assume that there are no errors at the start)
+        error_back = "#ffafaf"
+        has_errors = "no"
+
+        # change background to white (for testing purpouses)
+        self.start_amount_entry.config(bg="white")
+        self.amount_error_label.config(text="")
+
+        # disable all stakes buttons in case user changes mind and decreases amount entered
+        self.low_stakes_button.config(state=DISABLED)
+        self.medium_stakes_button.config(state=DISABLED)
+        self.high_stakes_button.config(state=DISABLED)
+
+        try:
+            starting_balance = int(starting_balance)
+
+        def to_game(self, stakes):
+
+            # retrieve starting balance
+            starting_balance = self.starting_funds.get()
+
+            # self.start_frame.destroy()
+            self.start_frame.destroy()
+            Game(self, stakes, starting_balance)
 
 
 # class Game
