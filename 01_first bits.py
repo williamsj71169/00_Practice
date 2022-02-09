9ca141f34e9f42a72b80cb4989c59f94adbde5df
williamsj71169 <58008516+williamsj71169@users.noreply.github.com>
2022-02-10 10:07:03 +1300

    1st

---
+++ 01_first bits.py
@@ -0,0 +1,32 @@
+
+# import
+
+
+class Start:
+    def __init__(self, parent):
+
+        # GUI to get starting balance and stakes
+        self.start_frame = Frame(padx=10, pady=10)
+        self.start_frame.grid()
+
+        # Set initial balance to zero
+        self.starting_funds - IntVar()
+        self.starting_funds.set(0)
+
+        # Mystery Heading (row 0)
+        self.mystery_box
+
+
+# class Game
+
+# class Help
+
+# class GameStats
+
+
+    # main routine
+if __name__ == "__main__":
+    root = Tk()
+    root.title("Mystery Box Game")
+    something = Start(root)
+    root.mainloop()
