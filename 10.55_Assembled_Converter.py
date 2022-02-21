c40761adbf9c69f9e8c2c504e666ffb2fe829a08
williamsj71169 <58008516+williamsj71169@users.noreply.github.com>
2022-02-21 14:49:59 +1300

    8

--- 10.55_Assembled_Converter.py
+++ 10.55_Assembled_Converter.py
@@ -204,6 +204,46 @@ class History:
         partner.history_button.config(state=NORMAL)
         self.history_box.destroy()
 
+
+class Help:
+    def __init__(self, partner):
+        background = "orange"
+
+        # disable help button
+        partner.help_button.config(state=DISABLED)
+
+        # sets up child window (ie: help box)
+        self.help_box = Toplevel()
+
+        # if users pres cross at top, closes help and 'releases' help button
+        self.help_box.protocol('WM_DELETE_WINDOW', partial(self.close_help, partner))
+
+        # set up GUI frame
+        self.help_frame = Frame(self.help_box, width=300, bg=background)
+        self.help_frame.grid()
+
+        # set ip Help heading (row 0)
+        self.how_heading = Label(self.help_frame, text="Help / Instructions",
+                                 font="arial 14 bold", bg=background)
+        self.how_heading.grid(row=0)
+
+        # help text (label, row 1)
+        self.help_text = Label(self.help_frame, text="",
+                               justify=LEFT, width=40, bg=background, wrap=250)
+        self.help_text.grid(row=1)
+
+        # dismiss button (row 2)
+        self.dismiss_btn = Button(self.help_frame, text="Dismiss",
+                                  width=10, bg="orange", font="arial 10 bold",
+                                  command=partial(self.close_help, partner))
+        self.dismiss_btn.grid(row=2, pady=10)
+
+    def close_help(self, partner):
+        # put help button back to normal
+        partner.help_button.config(state=NORMAL)
+        self.help_box.destroy()
+
+
 # main routine
 if __name__ == "__main__":
     root = Tk()
